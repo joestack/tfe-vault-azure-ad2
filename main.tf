dad489f9ac43d7b810c3b63703e1bb71783b6d86
joestack <joern@joern-online.de>
2019-05-22 12:16:48 +0200

    sn2

--- main.tf
+++ main.tf
@@ -26,16 +26,16 @@ module "network" {
                         }
 }
 
-module "ad-create" {
-  source  = "app.terraform.io/JoeStack/ad-create/azurerm"
-  version = "1.0.0"
-  admin_password = "${var.admin_password}"
-  admin_username = "${var.admin_username}"
-  location = "${var.location}"
-  prefix = "${var.prefix}"
-#  #private_ip_address = "${cidrhost(module.network.subnet1, 10)}"
-  private_ip_address = "10.0.1.10"
-  resource_group_name = "${var.resource_group_name}"
-  subnet_id = "${module.network.azurerm_subnet.subnet.0.id}"
-  #subnet_id = "${module.network.azurerm_subnet.0.id}"
-}
+#module "ad-create" {
+#  source  = "app.terraform.io/JoeStack/ad-create/azurerm"
+#  version = "1.0.0"
+#  admin_password = "${var.admin_password}"
+#  admin_username = "${var.admin_username}"
+#  location = "${var.location}"
+#  prefix = "${var.prefix}"
+##  #private_ip_address = "${cidrhost(module.network.subnet1, 10)}"
+#  private_ip_address = "10.0.1.10"
+#  resource_group_name = "${var.resource_group_name}"
+#  subnet_id = "${module.network.azurerm_subnet.subnet.0.id}"
+#  #subnet_id = "${module.network.azurerm_subnet.0.id}"
+#}
