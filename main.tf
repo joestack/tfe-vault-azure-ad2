b479c2c32328380e0e2d3454296d38c1a63a7e49
joestack <joern@joern-online.de>
2019-05-22 16:17:43 +0200

    sn8

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "network" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.2.0/24"]
+  subnet_prefixes     = ["10.0.1.0/24"]
   subnet_names        = ["subnet1"]
 
   tags                = {
@@ -26,19 +26,19 @@ module "network" {
                         }
 }
 
-#module "ad-create" {
-#  source  = "app.terraform.io/JoeStack/ad-create/azurerm"
-#  version = "1.0.0"
-#  admin_password = "${var.admin_password}"
-#  admin_username = "${var.admin_username}"
-#  location = "${var.location}"
-#  prefix = "${var.prefix}"
-##  #private_ip_address = "${cidrhost(module.network.subnet1, 10)}"
-#  private_ip_address = "10.0.1.10"
-#  resource_group_name = "${var.resource_group_name}"
-#  subnet_id = "${module.network.azurerm_subnet.subnet.0.id}"
-#  #subnet_id = "${module.network.azurerm_subnet.0.id}"
-#}
+module "ad-create" {
+  source  = "app.terraform.io/JoeStack/ad-create/azurerm"
+  version = "1.0.0"
+  admin_password = "${var.admin_password}"
+  admin_username = "${var.admin_username}"
+  location = "${var.location}"
+  prefix = "${var.prefix}"
+#  #private_ip_address = "${cidrhost(module.network.subnet1, 10)}"
+  private_ip_address = "10.0.1.10"
+  resource_group_name = "${var.resource_group_name}"
+  subnet_id = "${module.network.vnet_subnets[0]}"
+  #subnet_id = "${module.network.azurerm_subnet.0.id}"
+}
 
 output "sn_id" {
   value = "${module.network.vnet_subnets[0]}"
