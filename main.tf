2b92de986d967287e45a0d6fd7befa02b26caee1
joestack <joern@joern-online.de>
2019-05-22 16:14:59 +0200

    sn7

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "network" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_prefixes     = ["10.0.2.0/24"]
   subnet_names        = ["subnet1"]
 
   tags                = {
@@ -41,5 +41,5 @@ module "network" {
 #}
 
 output "sn_id" {
-  value = "${module.network.vnet_subnets}"
+  value = "${module.network.vnet_subnets[0]}"
 }
